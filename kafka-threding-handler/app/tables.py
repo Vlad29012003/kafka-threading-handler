899a74ebf6740fee5fcbcab14066a3d19df9b41e
Vlad <vladosmen29@gmail.com>
2026-09-17 20:11:01 +0600

    fix tables on SetTable before was Table

--- kafka-threding-handler/app/tables.py
+++ kafka-threding-handler/app/tables.py
@@ -1,5 +1,5 @@
 from .app import app
 
 
-blocked_users_table = app.Table("blocked_user", default=set, partitions=1)
-banned_words_table = app.Table("banned-words-table", default=set, partitions=1)
+blocked_users_table = app.SetTable("blocked_user", partitions=1)
+banned_words_table = app.SetTable("banned-words-table", partitions=1)
